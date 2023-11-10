416506045d0175b7588c13ee10afac89bf001fc3
vitinho231 <vitorzulmarsilva977@gmail.com>
2023-11-10 15:05:28 -0300

    atualização

--- libprg2/libprg2.h
+++ libprg2/libprg2.h
@@ -41,71 +41,7 @@ int buscaLinear(const Lista *lista, int valor);
 int buscaBinariaIterativa(const Lista *lista, int valor);
 int buscaBinariaRecursiva(const Lista *lista, int valor, int inicio, int fim);
 
-// Protótipos das funções
-void inicializaLista(Lista *lista, int tamanhoMax);
-int insereElemento(Lista *lista, int valor);
-int removeElemento(Lista *lista, int valor);
-void exibeLista(const Lista *lista);
-int buscaLinear(const Lista *lista, int valor);
-int buscaBinariaIterativa(const Lista *lista, int valor);
-int buscaBinariaRecursiva(const Lista *lista, int valor, int inicio, int fim);
 
-typedef struct Node {
-    int data;
-    struct Node *next;
-} Node;
 
-typedef struct {
-    Node *head;
-} ListaCircular;
-
-void inicializaListaCircular(ListaCircular *lista);
-void insereElementoCircular(ListaCircular *lista, int valor);
-void removeElementoCircular(ListaCircular *lista, int valor);
-void exibeListaCircular(ListaCircular *lista);
-
-typedef struct DNode {
-    int data;
-    struct DNode *prev;
-    struct DNode *next;
-} DNode;
-
-typedef struct {
-    DNode *head;
-} ListaDuplaCircular;
-
-void inicializaListaDuplaCircular(ListaDuplaCircular *lista);
-void insereElementoDuplaCircular(ListaDuplaCircular *lista, int valor);
-void removeElementoDuplaCircular(ListaDuplaCircular *lista, int valor);
-void exibeListaDuplaCircular(ListaDuplaCircular *lista);
-
-typedef struct QNode {
-    int data;
-    struct QNode *next;
-} QNode;
-
-typedef struct {
-    QNode *front;
-    QNode *rear;
-} FilaLista;
-
-void criaFilaLista(FilaLista *fila);
-void enfileirarLista(FilaLista *fila, int valor);
-int desenfileirarLista(FilaLista *fila);
-void imprimeFilaLista(const FilaLista *fila);
-
-typedef struct SNode {
-    int data;
-    struct SNode *next;
-} SNode;
-
-typedef struct {
-    SNode *top;
-} PilhaLista;
-
-void criaPilhaLista(PilhaLista *pilha);
-void empilharLista(PilhaLista *pilha, int valor);
-int desempilharLista(PilhaLista *pilha);
-void imprimePilhaLista(const PilhaLista *pilha);
 
 #endif // PRG029003_LIBPRG2_H
